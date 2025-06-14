0a132f65ac669ad4b177b42500ac0029c2c5ba0f
Augustus D'Souza <augustus@clergo.com>
2025-06-14 12:02:33 +0530

    Update force-same-tab.js

--- scripts/force-same-tab.js
+++ scripts/force-same-tab.js
@@ -5,12 +5,18 @@ const observer = new MutationObserver(() => {
     '#navigation-items a[target="_blank"][href="https://sanebase.dev"]'
   );
 
-  links.forEach((link) => {
+  for (const link of links) {
     if (link.textContent.includes("Back to")) {
+      // Remove target to stay in same tab
       link.removeAttribute("target");
       link.removeAttribute("rel");
+
+      // If docs are running on localhost, redirect link to local app
+      if (window.location.hostname === "localhost") {
+        link.setAttribute("href", "http://localhost:3000");
+      }
     }
-  });
+  }
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
